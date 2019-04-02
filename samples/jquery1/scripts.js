4e889833e38a00d404a8d34edeea78c64996366e
Maxim Fisher <mdf107@miami.edu>
2019-04-02 13:35:06 -0400

    js homework

---
+++ samples/jquery1/scripts.js
@@ -0,0 +1,27 @@
+$(function(){
+  //alert("jquery loaded!");
+});
+//functions could be click, mouseover, or mouseleave
+$('.hide').on('click',function(){
+  $('#myimage').hide();
+  $('#actiontitle').text('Image is hidden');
+  $('#actiontitle').css('color','red');
+
+})
+
+$('.show').on('click',function(){
+  $('#myimage').show();
+  $('#actiontitle').text('Image is hidden');
+  $('#actiontitle').css('color','green');
+
+})
+
+$('.done').on('click',function(){
+  $('body').html('<h2>We are done!</h2><p>See ya later!</p>')
+
+});
+
+$('.append').on('click',function(){
+  var username = $('#testinput').val();
+  $('#list').append('<li>'+username+'</li>');
+});
